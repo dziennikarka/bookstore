30592627e3c8f40a66b5e431a9a23582d38b4828
Irina Tregub <irinatregub@gmail.com>
2021-09-22 22:06:17 +0300

    Added deleting of a book

--- src/main/java/com/example/bookstore/web/bookController.java
+++ src/main/java/com/example/bookstore/web/bookController.java
@@ -5,6 +5,7 @@ import com.example.bookstore.domain.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,5 +24,11 @@ public class bookController {
 	public String getBookList(Model model){
 		model.addAttribute("books", repository.findAll());
 		return "booklist";
+	}
+
+	@RequestMapping(value="/delete/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") String isbn, Model model){
+		repository.deleteById(isbn);
+		return "redirect:/booklist";
 	}
 }
