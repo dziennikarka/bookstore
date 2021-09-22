13e835fc1f4937b10a3a46e6ae0514c5485abd32
Irina Tregub <irinatregub@gmail.com>
2021-09-22 18:47:55 +0300

    Added new endpoint in BookController /booklist

--- src/main/java/com/example/bookstore/web/bookController.java
+++ src/main/java/com/example/bookstore/web/bookController.java
@@ -1,14 +1,27 @@
 package com.example.bookstore.web;
 
 
+import com.example.bookstore.domain.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class bookController {
+
+	@Autowired
+	private BookRepository repository;
+
 	@RequestMapping(value="/index", method= RequestMethod.GET)
 	public String getBook(){
 		return "books";
+	}
+
+	@RequestMapping(value="/booklist", method=RequestMethod.GET)
+	public String getBookList(Model model){
+		model.addAttribute("books", repository.findAll());
+		return "booklist";
 	}
 }
