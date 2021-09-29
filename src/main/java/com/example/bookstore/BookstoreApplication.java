19d6c1ca6b9098eb687ae1604732c92a2062a856
Irina Tregub <irinatregub@gmail.com>
2021-09-29 22:40:49 +0300

    Added Category, CategoryRepository and CommandLineRunner

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,13 +1,30 @@
 package com.example.bookstore;
 
+import com.example.bookstore.domain.Category;
+import com.example.bookstore.domain.CategoryRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(CategoryRepository repository){
+		return(args) -> {
+			Category first = new Category("scifi");
+			Category second = new Category ("drama");
+			Category third = new Category ("comic");
+
+			repository.save(first);
+			repository.save(second);
+			repository.save(third);
+		};
 	}
 
 }
---
+++ src/main/java/com/example/bookstore/domain/Category.java
@@ -0,0 +1,49 @@
+package com.example.bookstore.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+
+    public Category() {
+        this.id = null;
+        this.name = null;
+
+    }
+
+    public Category(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
