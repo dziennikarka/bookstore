04d77d4225b3fd0f066d9a576de7bcdeca6ce43f
Irina Tregub <irinatregub@gmail.com>
2022-01-04 17:26:40 +0200

    Added smoke testing for all controllers

--- src/test/java/com/example/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/example/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,37 @@
 package com.example.bookstore;
 
+import com.example.bookstore.web.BookController;
+import com.example.bookstore.web.CategoryController;
+import com.example.bookstore.web.UserController;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
 
+/**
+ * Smoke testing: testing that controllers are created
+ */
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class BookstoreApplicationTests {
 
+	@Autowired
+	private BookController bookController;
+
+	@Autowired
+	private CategoryController categoryController;
+
+	@Autowired
+	private UserController userController;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(bookController).isNotNull();
+		assertThat(categoryController).isNotNull();
+		assertThat(userController).isNotNull();
 	}
 
 }
